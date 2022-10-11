112b9ef42603d72dcf34818360930d38356c59e1
Bijendra2022 <100510660+Bijendra2022@users.noreply.github.com>
2022-10-11 11:22:40 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@ data "aws_iam_instance_profile" "instance_profile" {
   name = "ec2"
 }
 data "aws_availability_zone" "az" {
-  name                   = "us-east-1a"
+  name                   = "ap-south-1a"
   all_availability_zones = false
   state                  = "available"
 }
@@ -10,8 +10,8 @@ data "aws_key_pair" "key" {
   key_name = "dev-account"
 }
 data "aws_subnet" "selected" {
-  vpc_id            = "vpc-0dc8618be1f1b3e2d"
-  availability_zone = "us-east-1a"
+  vpc_id            = "vpc-08b795f373d5afda8"
+  availability_zone = "ap-south-1a"
   filter {
     name   = "tag:Name"
     values = ["tcw_public_subnet_az_1a"]
@@ -19,6 +19,6 @@ data "aws_subnet" "selected" {
 }
 data "aws_security_groups" "sg" {
   tags = {
-    Name = "tcw_security_group"
+    Name = "sg-01fce448a6f54bb61"
   }
 }
